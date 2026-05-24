91223f759c155091e2d1d11efe2436d2df5414d0
ralf <ralf@Americium>
2026-05-24 15:32:02 +0200

    Test changed

--- src/main/java/com/ralf/asac/Asac.java
+++ src/main/java/com/ralf/asac/Asac.java
@@ -5,6 +5,7 @@ public class Asac {
 	static final String DEFAULT_SOUND_FILE = "default.mp3";
 
 	public static void main(String[] args) {
+		System.out.println("asdafggc start ...");
 		MainClass.main(args);
 	}
 }
